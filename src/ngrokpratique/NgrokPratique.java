1d9cc58c98cd0482d5a623cec27102efd55f3217
Ana-ramahef <59668599+Ana-ramahef@users.noreply.github.com>
2021-10-05 23:00:09 -0400

    au revoir

--- src/ngrokpratique/NgrokPratique.java
+++ src/ngrokpratique/NgrokPratique.java
@@ -16,6 +16,7 @@ public class NgrokPratique {
      */
     public static void main(String[] args) {
         System.out.println("Bonjour "+args[1]+", " +args[0]);
+        System.out.println("Au revoir");
     }
     
 }
